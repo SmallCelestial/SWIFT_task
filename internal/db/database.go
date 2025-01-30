264cee3f539cf53b934b38998a369ea57e394c30
Mateusz <mateusz.oracz2001@gmail.com>
2025-01-30 17:27:11 +0100

    added database init

--- internal/db/database.go
+++ internal/db/database.go
@@ -1,1 +1,37 @@
 package db
+
+import (
+	"SWIFT_task/internal/model"
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	//host := os.Getenv("DB_HOST")
+	//user := os.Getenv("DB_USER")
+	//password := os.Getenv("DB_PASSWORD")
+	//dbname := os.Getenv("DB_NAME")
+	//port := "5432"
+	host := "localhost"
+	user := "postgres"
+	password := "admin"
+	dbname := "swift_task"
+	port := "5432"
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, port)
+
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	err = DB.AutoMigrate(&model.Branch{}, &model.BranchRelationship{})
+	if err != nil {
+		log.Fatalf("Migration failed: %v", err)
+	}
+}
--- internal/model/swift_code.go
+++ internal/model/swift_code.go
@@ -1,11 +1,22 @@
 package model
 
 type Branch struct {
-	Address       string   `json:"address"`
-	BankName      string   `json:"bankName"`
-	CountryISO2   string   `json:"countryISO2"`
-	CountryName   string   `json:"countryName"`
-	SwiftCode     string   `json:"swiftCode"`
-	IsHeadquarter bool     `json:"isHeadquarter"`
-	Branches      []Branch `json:"branches,omitempty"`
+	Address       string `gorm:"size:255"`
+	BankName      string `gorm:"size:255"`
+	CountryISO2   string `gorm:"size:2"`
+	CountryName   string `gorm:"size:255"`
+	SwiftCode     string `gorm:"size:11;primaryKey"`
+	IsHeadquarter bool   `gorm:"default:false"`
+}
+
+type BranchRelationship struct {
+	HeadquarterSwiftCode    string `gorm:"size:11;primaryKey"`
+	OrdinaryBranchSwiftCode string `gorm:"size:11;primaryKey"`
+
+	HeadquarterBranch *Branch `gorm:"foreignKey:HeadquarterSwiftCode;references:SwiftCode"`
+	OrdinaryBranch    *Branch `gorm:"foreignKey:OrdinaryBranchSwiftCode;references:SwiftCode"`
+}
+
+func (b Branch) GetHeadQuarterSwiftCode() string {
+	return b.SwiftCode[:8] + "XXX"
 }
